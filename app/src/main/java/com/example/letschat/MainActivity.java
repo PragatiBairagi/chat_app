478485e18eab973955cad985c8dfd98bf8974583
pragati <pragati12bairagi@gmail.com>
2023-12-15 01:37:38 +0530

    in process

---
+++ app/src/main/java/com/example/letschat/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.letschat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
